159a0f4a3d0324bb0be719c52d46074e7a878a7b
consuelita <adelita@operatorfoundation.org>
2024-02-29 19:06:46 -0600

    Update ReplicantConfig.kt

--- Replicant/src/main/java/org/operatorfoundation/replicantandroid/ReplicantConfig.kt
+++ Replicant/src/main/java/org/operatorfoundation/replicantandroid/ReplicantConfig.kt
@@ -8,17 +8,17 @@ import org.operatorfoundation.transmission.TransmissionConnection
 import java.util.logging.Logger
 
 @Serializable
-class ReplicantConfig(val serverAddress: String, val polish: PolishConfig, val toneburst: ToneBurst, val transport: String)
+class ReplicantConfig(val serverAddress: String, val polish: PolishConfig, val toneburst: ToneBurst, val transport: String = "replicant")
 {
     fun connect(context: Context, logger: Logger?): Connection {
         val hostPort = serverAddress.split(":")
         val host = hostPort[0]
         val port = hostPort[1].toInt()
         val connection = TransmissionConnection(host, port, ConnectionType.TCP,null)
-        return connect(context, logger, connection)
+        return connect(connection, context, logger)
     }
 
-    fun connect(context: Context, logger: Logger?, connection: Connection): Connection {
+    fun connect(connection: Connection, context: Context, logger: Logger?): Connection {
         toneburst.perform(connection)
         return polish.polish(connection, context, logger)
     }
